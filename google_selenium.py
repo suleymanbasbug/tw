0f91ae4c60aec0d3272e1d74d4b3eb80519c8803
Süleyman Başbuğ <suleymanbasbug@hotmail.com>
2025-10-07 15:22:30 +0300

    init

--- google_selenium.py
+++ google_selenium.py
@@ -1,16 +1,59 @@
 import time
-from seleniumbase import Driver
+from seleniumbase import BaseCase
 
-driver = Driver(headless=False, proxy="3ed492ea9d26670b06c1__cr.us:4e17d20cd644f516@gw.dataimpulse.com:823", locale="en")
-driver.get("https://x.com/i/flow/signup")
-time.sleep(6)
+class TwitterSignup(BaseCase):
+    def test_signup(self):
+        # İngilizce locale ile başlat
+        self.open("https://x.com/i/flow/signup")
+        time.sleep(6)
 
-# Create account butonuna tıkla
-print("Create account butonuna tıklanıyor...")
-driver.click('button:contains("Create account")')
-print("Butona tıklandı!")
+        # Create account butonuna tıkla
+        self.click('button:contains("Create account")')
+        print("Create account butonuna tıklandı!")
 
-# Enter'a basılmasını bekle
-input("Çıkmak için Enter'a basın...")
+        # Sayfanın yüklenmesini bekle
+        time.sleep(3)
 
-driver.quit()
+        # Use email instead butonuna tıkla
+        self.click('button:contains("Use email instead")')
+        print("Use email instead butonuna tıklandı!")
+
+        # Sayfanın yüklenmesini bekle
+        time.sleep(3)
+
+        # Name input'una "süleyman" yaz
+        self.type('input[name="name"]', "süleyman")
+        time.sleep(1)
+        print("Name input'una 'süleyman' yazıldı!")
+
+        # Email input'una "sede@sede.com" yaz
+        self.type('input[name="email"]', "sede@sede.com")
+        time.sleep(1)
+        print("Email input'una 'sede@sede.com' yazıldı!")
+
+        # Ay seçimi - June (6. ay) seç
+        self.select_option_by_text('select[id="SELECTOR_1"]', "June")
+        time.sleep(1)
+        print("Ay olarak 'June' seçildi!")
+
+        # Gün seçimi - 30. gün seç
+        self.select_option_by_text('select[id="SELECTOR_2"]', "30")
+        time.sleep(1)
+        print("Gün olarak '30' seçildi!")
+
+        # Yıl seçimi - 1994 seç
+        self.select_option_by_text('select[id="SELECTOR_3"]', "1994")
+        time.sleep(1)
+        print("Yıl olarak '1994' seçildi!")
+
+        # Next butonuna tıkla
+        self.click('button:contains("Next")')
+        time.sleep(1)
+        print("Next butonuna tıklandı!")
+
+        # Enter'a basılmasını bekle
+        input("Çıkmak için Enter'a basın...")
+
+if __name__ == "__main__":
+    # Proxy, İngilizce locale ve headed mode ile çalıştır
+    BaseCase.main(__name__, __file__, "--proxy=3ed492ea9d26670b06c1__cr.us:4e17d20cd644f516@gw.dataimpulse.com:823", "--locale=en", "--headed")
